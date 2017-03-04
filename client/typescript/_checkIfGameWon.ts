82e274f4f6f7ce6b01f657c4338784a16b4dcab1
Pavle <V381@users.noreply.github.com>
2017-03-04 18:01:59 +0100

    prevent Notice Bug

--- client/typescript/_checkIfGameWon.ts
+++ client/typescript/_checkIfGameWon.ts
@@ -5,6 +5,10 @@ export class CheckIfGameWon{
 
     checkIfAllLetersAreOnLines(letters):void{
         // Wait for letter to come to lines
+        // If textIndent is on 0, that means that letter is on line, then increase letterCounter
+        // If letterCounter matches the lenght of length, we show winning notice!
+        // We reset letterCounter to 0 everytime winning notice is shown to prevent Bug
+
         setTimeout(function () {
             if (letters.style.textIndent === "0px") {
                 this.letterCounter++;
@@ -16,9 +20,8 @@ export class CheckIfGameWon{
     showWinNotice():void{
         if(this.letterCounter === this.letters.length){
             this.gameWinNotice.style.display = "block";
+            this.letterCounter = 0;
         }
     }
-
-
 
 }
