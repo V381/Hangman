abb8039216d597c9da4d4d89317d97440dfdc2bf
Pavle <V381@users.noreply.github.com>
2017-03-05 01:47:59 +0100

    Variables that count how many letter were added to lines and how much of hangman body is drawed

---
+++ client/typescript/_victoryCounter.js
@@ -0,0 +1,3 @@
+// Variables that count how many letter were added to lines and how much of hangman body is drawed
+var letterCounter = 0;
+var hangManBodyCounter = 0;
---
+++ client/typescript/_victoryCounter.ts
@@ -0,0 +1,4 @@
+// Variables that count how many letter were added to lines and how much of hangman body is drawed
+
+let letterCounter = 0;
+let hangManBodyCounter = 0;
