055b27ff13d8983258e5da7326d9ac725305cf2d
Pavle <V381@users.noreply.github.com>
2017-03-05 01:56:08 +0100

    Moved to app.js

--- client/typescript/_startNewGame.ts
+++ client/typescript/_startNewGame.ts
@@ -1,56 +1,58 @@
-import { GenerateAlphabet } from './_generateAlphabet.js';
-import { GenerateWords } from './_generateWords.js';
-import { CheckForLetters } from './_checkForLetters.js';
-import { GenerateHangman} from './_generateHangman.js';
-import { CheckIfGameIsLost } from './_checkIfGameIsLost.js';
+// import { GenerateAlphabet } from './_generateAlphabet.js';
+// import { GenerateWords } from './_generateWords.js';
+// import { CheckForLetters } from './_checkForLetters.js';
+// import { GenerateHangman} from './_generateHangman.js';
+// import { CheckIfGameIsLost } from './_checkIfGameIsLost.js';
+// import { CheckIfGameWon } from './_checkIfGameWon.js';
 
 
-export class StartNewGame{
-    lostButton = document.querySelector('.lost__button');
-    winButton = document.querySelector('.win__button');
-    gameNoticeLost = document.querySelector('.gameNotice__lost');
-    gameNoticeWon = document.querySelector('.gameNotice__win');
+// export class StartNewGame{
+//     lostButton = document.querySelector('.lost__button');
+//     winButton = document.querySelector('.win__button');
+//     gameNoticeLost = document.querySelector('.gameNotice__lost');
+//     gameNoticeWon = document.querySelector('.gameNotice__win');
 
-    generateWords = new GenerateWords();
-    generateAlphabet = new GenerateAlphabet();
-    generateHangman = new GenerateHangman();
-    checkForLetters  = new CheckForLetters();
-    checkIfGameLost = new CheckIfGameIsLost();
+//     generateWords = new GenerateWords();
+//     generateAlphabet = new GenerateAlphabet();
+//     generateHangman = new GenerateHangman();
+//     checkForLetters  = new CheckForLetters();
+//     checkIfGameLost = new CheckIfGameIsLost();
+//     checkIfGameWon = new CheckIfGameWon();
 
-    startNewGameIfLost(){
-        this.lostButton.addEventListener('click', () => {
-            this.gameNoticeLost.style.display = 'none;'
-            this.generateWords.generateEnglishWords();
-            this.generateAlphabet.generateAlphabetButtons();
-            this.generateHangman.hideBodyParts();
-            this.checkForLetters.checkForLetterMatch(); 
-            this.removeGameNoticeLost();
-        });
-    }
-
-
-
-    removeGameNoticeLost(){
-        this.gameNoticeLost.removeAttribute('style');
-        this.gameNoticeLost.setAttribute('display', 'none')
-    }
-
-    startNewGameIfWon(){
-        this.winButton.addEventListener('click', () => {
-            this.gameNoticeWon.style.display = 'none;'
-            this.generateWords.generateEnglishWords();
-            this.generateAlphabet.generateAlphabetButtons();
-            this.generateHangman.hideBodyParts();
-            this.checkForLetters.checkForLetterMatch(); 
-            this.removeGameNoticeWon();
-        });
-    }
-
-    removeGameNoticeWon(){
-        this.gameNoticeWon.removeAttribute('style');
-        this.gameNoticeWon.setAttribute('display', 'none')
-    }
+//     startNewGameIfLost(){
+//         this.lostButton.addEventListener('click', () => {
+//             this.gameNoticeLost.style.display = 'none;'
+//             this.generateAlphabet.generateAlphabetButtons();
+//             this.generateWords.generateEnglishWords();
+//             this.generateHangman.hideBodyParts();
+//             this.checkForLetters.checkForLetterMatch(); 
+//             this.removeGameNoticeLost();
+//         });
+//     }
 
 
 
-}
+//     removeGameNoticeLost(){
+//         this.gameNoticeLost.removeAttribute('style');
+//         this.gameNoticeLost.setAttribute('display', 'none')
+//     }
+
+//     startNewGameIfWon(){
+//         this.winButton.addEventListener('click', () => {
+//             this.gameNoticeWon.style.display = 'none;'
+//             this.generateWords.generateEnglishWords();
+//             this.generateAlphabet.generateAlphabetButtons();
+//             this.generateHangman.hideBodyParts();
+//             this.checkForLetters.checkForLetterMatch(); 
+//             this.removeGameNoticeWon();
+//         });
+//     }
+
+//     removeGameNoticeWon(){
+//         this.gameNoticeWon.removeAttribute('style');
+//         this.gameNoticeWon.setAttribute('display', 'none')
+//     }
+
+
+
+// }
